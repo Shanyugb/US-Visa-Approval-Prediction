1c68041fd41f5d07b98f252e069029e969f23f9e
Prathik Jain <prathik.jain.gn@gmail.com>
2024-04-10 04:55:45 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Prathik Jain",
+    author_email="prathik.jain.gn@gmail.com",
+    packages=find_packages()
+)
